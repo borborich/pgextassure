68630aa6e455843276ce6f15684eab3c5b3b5123
borborich <38986985+borborich@users.noreply.github.com>
2026-07-29 17:31:01 +0200

    Prepare v0.1.0-alpha.8 release

--- src/pgextassure/_build_backend.py
+++ src/pgextassure/_build_backend.py
@@ -20,7 +20,7 @@ import zipfile
 
 
 NAME = "pgextassure"
-VERSION = "0.1.0a7"
+VERSION = "0.1.0a8"
 DIST_INFO = f"{NAME}-{VERSION}.dist-info"
 WHEEL_NAME = f"{NAME}-{VERSION}-py3-none-any.whl"
 SDIST_NAME = f"{NAME}-{VERSION}.tar.gz"
--- src/pgextassure/_version.py
+++ src/pgextassure/_version.py
@@ -1,4 +1,4 @@
 """Canonical package and public release version identifiers."""
 
-PACKAGE_VERSION = "0.1.0a7"
-RELEASE_VERSION = "0.1.0-alpha.7"
+PACKAGE_VERSION = "0.1.0a8"
+RELEASE_VERSION = "0.1.0-alpha.8"
